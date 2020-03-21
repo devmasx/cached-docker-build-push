9c70612cb9eb865a4b082e8001c4b9c732eb6105
miguemasx <migue.masx@gmail.com>
2020-03-21 03:36:53 +0100

    autocommit

--- auto-commit.js
+++ auto-commit.js
@@ -1,72 +1,70 @@
 const { Octokit } = require("@octokit/rest");
+const exec = require('@actions/exec');
+
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-
-
+// https://developer.github.com/v3/repos/commits/
+// https://developer.github.com/v3/git/trees/
+// https://developer.github.com/v3/git/refs/#update-a-reference
 // git diff --name-only HEAD~1 HEAD~2
 
-const owner = "devmasx";
-const repo = "cached-docker-build-push";
+const getFilesChanges = async () => {
+  let myOutput = '';
+  const options = {
+    silent: true,
+    listeners: {
+      stdout: (data) => {
+        myOutput += data.toString();
+      }
+    }
+  };
+  const result = await exec.exec('git', ["diff", "--name-only", "HEAD~1", "HEAD~2"], options);
+  return myOutput.split("\n").filter(it => it)
+};
 
 (async () => {
-
-  // const response4 = await octokit.request("GET /repos/:owner/:repo/git/refs/:ref", {
-  //   owner,
-  //   repo,
-  //   ref: "heads/test-commit",
-  // });
-
-  // console.log(response4.data)
-
-  const tree = [{
-    path: 'new_file.txt',
-    mode: '100644',
-    content: 'My new file'
-  }];
-
-  // Get latest commit and tree
-  const response = await octokit.request("GET /repos/:owner/:repo/commits/a81eb661e79d84ae116cbfaff94f9f60502b50a9", { owner, repo });
-  const tree_sha = response.data.commit.tree.sha
-  // const latest_commit_sha = response.data.sha;
-  const latest_commit_sha = "a81eb661e79d84ae116cbfaff94f9f60502b50a9";
-
-  console.log(tree_sha)
-  console.log(latest_commit_sha)
-  // Save changes
-  const response2 = await octokit.request("POST /repos/:owner/:repo/git/trees", { owner, repo, base_tree: tree_sha, tree })
-  const new_tree_sha = response2.data.sha;
-
-  // Commit changes
-  const response3 = await octokit.request("POST /repos/:owner/:repo/git/commits", {
-    owner,
-    repo,
-    message: 'Commit message',
-    tree: new_tree_sha,
-    parents: [latest_commit_sha]
-  });
-  console.log(response3.data.sha)
-  console.log(response3.data.url)
-  console.log(response3.data.message)
-
-
-  // git rev-parse --abbrev-ref HEAD
-  // Update branch (push)
-  const response4 = await octokit.request("PATCH /repos/:owner/:repo/git/refs/:ref", {
-    owner,
-    repo,
-    ref: "heads/test-commit",
-    sha: response3.data.sha,
-  })
-
-  // console.log(response4.data)
+  await getFilesChanges().then(console.log)
 })()
 
-// response = client.get("", sha: base, per_page: 1)
-// tree_sha = response[0].commit.tree.sha
-// latest_commit_sha = response[0].sha
+// (async () => {
+//   const owner = "devmasx";
+//   const repo = "cached-docker-build-push";
+//   const brachName = "test-commit"
+//   const commitMessage = "Commit message"
 
-// response = client.post("/repos/#{owner}/#{repo}/git/trees", base_tree: tree_sha, tree: tree)
-// new_tree_sha = response.sha
+//   const tree = [{
+//     path: 'new_file.txt',
+//     mode: '100644',
+//     content: 'My new file 3'
+//   }];
 
-// response = client.post("/repos/#{owner}/#{repo}/git/commits",
-//                        message: changes[:commit], tree: new_tree_sha, parents: [latest_commit_sha])
-// latest_commit_sha = response.sha
+//   const response0 = await octokit.request("GET /repos/:owner/:repo/commits", { owner, repo, sha: brachName });
+//   const latestCommitSHA = response0.data[0].sha
+
+//   // Get latest commit and tree
+//   const response1 = await octokit.request(`GET /repos/:owner/:repo/commits/${latestCommitSHA}`, { owner, repo });
+//   const base_tree = response1.data.commit.tree.sha
+
+//   // Save changes
+//   const response2 = await octokit.request("POST /repos/:owner/:repo/git/trees", { owner, repo, base_tree, tree })
+//   const new_tree_sha = response2.data.sha;
+
+//   // Commit changes
+//   const response3 = await octokit.request("POST /repos/:owner/:repo/git/commits", {
+//     owner,
+//     repo,
+//     message: commitMessage,
+//     tree: new_tree_sha,
+//     parents: [latestCommitSHA]
+//   });
+
+//   // git rev-parse --abbrev-ref HEAD
+//   // Update branch (push)
+//   const response4 = await octokit.request("PATCH /repos/:owner/:repo/git/refs/:ref", {
+//     owner,
+//     repo,
+//     ref: `heads/${brachName}`,
+//     sha: response3.data.sha,
+//   })
+
+//   console.log(response4.data)
+// })()
