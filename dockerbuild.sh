56789ad99825b98307de51fe53a1e53277090ff6
Miguel Savignano <migue.masx@gmail.com>
2020-06-21 18:24:40 +0200

    .

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,27 +1,27 @@
-string=""
+PARAMS=""
 
 if [ ! -z $INPUT_IMAGE_NAME ]; then
-  string+=" --image-name $INPUT_IMAGE_NAME"
+  PARAMS+=" --image-name $INPUT_IMAGE_NAME"
 fi
 
 if [ ! -z $INPUT_IMAGE_TAG ]; then
-  string+=" --image-tag $INPUT_IMAGE_TAG"
+  PARAMS+=" --image-tag $INPUT_IMAGE_TAG"
 fi
 
 if [ ! -z $INPUT_FILE ]; then
-  string+=" --file $INPUT_FILE"
+  PARAMS+=" --file $INPUT_FILE"
 fi
 
 if [ ! -z $INPUT_BUILD_PARAMS ]; then
-  string+=" --build-params $INPUT_BUILD_PARAMS"
+  PARAMS+=" --build-params $INPUT_BUILD_PARAMS"
 fi
 
 if [ ! -z $INPUT_PRINT ]; then
-  string+=" --print"
+  PARAMS+=" --print"
 fi
 
 if [ ! -z $INPUT_PUSH ]; then
-  string+=" --push"
+  PARAMS+=" --push"
 fi
 
-echo cached-docker $string
+echo cached-docker $PARAMS
