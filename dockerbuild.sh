8bc25ead6a067cd0453ee0238f223db3a397fced
Miguel Savignano <migue.masx@gmail.com>
2020-06-21 19:24:00 +0200

    .

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,6 +1,6 @@
 PARAMS=""
 echo $INPUT_IMAGE_NAME
-[ ! -z "$INPUT_IMAGE_NAME" ] && PARAMS+=" --image-name=$INPUT_IMAGE_NAME"
+[ ! -z "$INPUT_IMAGE_NAME" ] && PARAMS+="--image-name=$INPUT_IMAGE_NAME "
 
 # if [ ! -z  ]; then
 #   # PARAMS+=" --image-name $INPUT_IMAGE_NAME"
