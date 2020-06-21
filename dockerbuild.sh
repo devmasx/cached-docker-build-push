2eb74ff146f226f3a639b1e76eed8c8d094e42b8
Miguel Savignano <migue.masx@gmail.com>
2020-06-21 17:10:01 +0200

    fix call empty flag

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -3,7 +3,6 @@ if [ ! -z $INPUT_PRINT ]; then
    --image-name $INPUT_IMAGE_NAME \
    --image-tag $INPUT_IMAGE_TAG \
    --build-params $INPUT_BUILD_PARAMS \
-   --file $INPUT_FILE \
    --cache-stage-target $INPUT_CACHE_STAGE_TARGET \
    --print
 else
@@ -11,6 +10,5 @@ else
    --image-name $INPUT_IMAGE_NAME \
    --image-tag $INPUT_IMAGE_TAG \
    --build-params $INPUT_BUILD_PARAMS \
-   --file $INPUT_FILE \
    --cache-stage-target $INPUT_CACHE_STAGE_TARGET
 fi
