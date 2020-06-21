e778c61d1942c2f8571b023961669791a6c177f7
Miguel Savignano <migue.masx@gmail.com>
2020-06-21 18:30:45 +0200

    .

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,28 +1,29 @@
 PARAMS=""
 
-if [ ! -z $INPUT_IMAGE_NAME ]; then
-  echo $INPUT_IMAGE_NAME
-  # PARAMS+=" --image-name $INPUT_IMAGE_NAME"
-fi
+[ ! -z "$INPUT_IMAGE_NAME" ] && PARAMS+=" --image-name=$INPUT_IMAGE_NAME"
 
-if [ ! -z $INPUT_IMAGE_TAG ]; then
-  # PARAMS+=" --image-tag $INPUT_IMAGE_TAG"
-fi
+# if [ ! -z  ]; then
+#   # PARAMS+=" --image-name $INPUT_IMAGE_NAME"
+# fi
 
-if [ ! -z $INPUT_FILE ]; then
-  # PARAMS+=" --file $INPUT_FILE"
-fi
+# if [ ! -z $INPUT_IMAGE_TAG ]; then
+#   # PARAMS+=" --image-tag $INPUT_IMAGE_TAG"
+# fi
 
-if [ ! -z $INPUT_BUILD_PARAMS ]; then
-  # PARAMS+=" --build-params $INPUT_BUILD_PARAMS"
-fi
+# if [ ! -z $INPUT_FILE ]; then
+#   # PARAMS+=" --file $INPUT_FILE"
+# fi
 
-if [ ! -z $INPUT_PRINT ]; then
-  # PARAMS+=" --print"
-fi
+# if [ ! -z $INPUT_BUILD_PARAMS ]; then
+#   # PARAMS+=" --build-params $INPUT_BUILD_PARAMS"
+# fi
 
-if [ ! -z $INPUT_PUSH ]; then
-  # PARAMS+=" --push"
-fi
+# if [ ! -z $INPUT_PRINT ]; then
+#   # PARAMS+=" --print"
+# fi
 
-echo cached-docker --help
+# if [ ! -z $INPUT_PUSH ]; then
+#   # PARAMS+=" --push"
+# fi
+
+echo cached-docker $PARAMS
