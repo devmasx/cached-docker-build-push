fdd8bafb9729b25d40ec9315331484eb66e6dd47
Miguel Savignano <migue.masx@gmail.com>
2020-06-21 17:34:12 +0200

    only build

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,14 +1,17 @@
-if [ ! -z $INPUT_PRINT ]; then
+# if [ -z $INPUT_PRINT ] && [ -z $INPUT_FILE ]; then
   cached-docker \
    --image-name $INPUT_IMAGE_NAME \
    --image-tag $INPUT_IMAGE_TAG \
    --build-params $INPUT_BUILD_PARAMS \
-   --cache-stage-target $INPUT_CACHE_STAGE_TARGET \
-   --print
-else
-  cached-docker \
-   --image-name $INPUT_IMAGE_NAME \
-   --image-tag $INPUT_IMAGE_TAG \
-   --build-params $INPUT_BUILD_PARAMS \
-   --cache-stage-target $INPUT_CACHE_STAGE_TARGET
-fi
+  #  --push
+  #  --cache-stage-target $INPUT_CACHE_STAGE_TARGET \
+# else
+  # cached-docker \
+  #  --image-name $INPUT_IMAGE_NAME \
+  #  --image-tag $INPUT_IMAGE_TAG \
+  #  --build-params $INPUT_BUILD_PARAMS \
+  #  --cache-stage-target $INPUT_CACHE_STAGE_TARGET
+  #  --file
+  #  --push
+  #  --print
+# fi
