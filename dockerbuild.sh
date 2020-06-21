d375fb35806736ec1538ad93131cac508ed7060d
Miguel Savignano <migue.masx@gmail.com>
2020-06-21 18:11:49 +0200

    build cli arguments,

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,20 +1,27 @@
-if [ -z $INPUT_FILE ]; then
-  DOCKERFILE_PATH="./Dockerfile"
-else
-  DOCKERFILE_PATH=$INPUT_FILE
+args=""
+
+if [ ! -z $INPUT_IMAGE_NAME ]; then
+  args+=" --image-name ${INPUT_IMAGE_NAME}"
 fi
 
-if [ -z $INPUT_PRINT ]; then
-  cached-docker \
-   --image-name $INPUT_IMAGE_NAME \
-   --image-tag $INPUT_IMAGE_TAG \
-   --file $DOCKERFILE_PATH \
-   --push
-else
-  cached-docker \
-   --image-name $INPUT_IMAGE_NAME \
-   --image-tag $INPUT_IMAGE_TAG \
-   --file $DOCKERFILE_PATH \
-   --push
-   --print
+if [ ! -z $INPUT_IMAGE_TAG ]; then
+  args+=" --image-tag ${INPUT_IMAGE_TAG}"
 fi
+
+if [ ! -z $INPUT_FILE ]; then
+  args+=" --file ${INPUT_FILE}"
+fi
+
+if [ ! -z $INPUT_BUILD_PARAMS ]; then
+  args+=" --build-params ${INPUT_BUILD_PARAMS}"
+fi
+
+if [ ! -z $INPUT_PRINT ]; then
+  args+=" --print"
+fi
+
+if [ ! -z $INPUT_PUSH ]; then
+  args+=" --push"
+fi
+
+cached-docker ${args}
